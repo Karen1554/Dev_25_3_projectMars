efd2319dfda02d667cbe2bc2df864750f63cc1c0
sigmotoa <si.galvismotoa@gmail.com>
2025-10-22 07:21:23 -0500

    Adding the saving of img

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.staticfiles import StaticFiles
 import appoinment
 import pet
 import estudiantes
 import user
 import vet
+import images
 from db import create_tables
 ##from pet import APIRouter
 
@@ -15,11 +16,20 @@ async def lifespan(app: FastAPI):
     await create_tables(app)
     yield
 app = FastAPI(lifespan=lifespan, title="Pet API")
+
+app.mount("/img", StaticFiles(directory="upload"), name="img")
 app.include_router(pet.router, tags=["pet"], prefix="/pets")
 app.include_router(user.router, tags=["user"], prefix="/users")
 app.include_router(vet.router, tags=["vet"], prefix="/vets")
 app.include_router(appoinment.router, tags=["appointment"], prefix="/appointments")
 #app.include_router(estudiantes.router, tags=["estudiantes"], prefix="/estudiantes" )
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    result = await images.upload_file(file)
+    return result
+
 
 
 @app.get("/")

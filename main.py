27f72fa007c99528d45e643bfa2a346abff880f5
sigmotoa <si.galvismotoa@gmail.com>
2025-10-22 13:42:50 -0500

    Creation of the bucket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.staticfiles import StaticFiles
 
+import bucket
 import appoinment
 import pet
 import estudiantes
@@ -22,11 +24,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="Pet API")
+
+app.mount("/static", StaticFiles(directory="bucket"), name="static")
 app.include_router(pet.router, tags=["pet"], prefix="/pets")
 app.include_router(user.router, tags=["user"], prefix="/users")
 app.include_router(vet.router, tags=["vet"], prefix="/vets")
 app.include_router(appoinment.router, tags=["appointment"], prefix="/appointments")
 #app.include_router(estudiantes.router, tags=["estudiantes"], prefix="/estudiantes" )
+
+@app.post("/bucket")
+async def save_bucket (file: UploadFile = File(...)):
+    result = await bucket.upload_file(file)
+    return result
 
 
 @app.get("/")

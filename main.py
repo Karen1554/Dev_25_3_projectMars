8da508651f8354ca750d649ecaf0a722d4caf94c
sigmotoa <si.galvismotoa@gmail.com>
2025-11-06 10:11:22 -0500

    Adding html using jinja.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, Request
 from fastapi.staticfiles import StaticFiles
 import appoinment
 import pet
@@ -22,12 +23,15 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, title="Pet API")
 
+
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 app.mount("/img", StaticFiles(directory="upload"), name="img")
 app.include_router(pet.router, tags=["pet"], prefix="/pets")
 app.include_router(user.router, tags=["user"], prefix="/users")
 app.include_router(vet.router, tags=["vet"], prefix="/vets")
 app.include_router(appoinment.router, tags=["appointment"], prefix="/appointments")
 
+templates = Jinja2Templates(directory="templates")
 
 # app.include_router(estudiantes.router, tags=["estudiantes"], prefix="/estudiantes" )
 
@@ -39,18 +43,10 @@ async def upload_file(file: UploadFile = File(...)):
 
 
 @app.get("/", response_class=HTMLResponse, status_code=200)
-async def root():
-    return """
-    <html>
-    <head>
-    </head>
-    <body>
-    <h1>Pet API</h1>
-    </body>
-    </html>
-    
-    """
-
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request = request, name="index.html"
+    )
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
